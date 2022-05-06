3978cae1a6b59f0a80c309ca9c563ad1a5c26dce
semyeongJ <94214574+semyeongJ@users.noreply.github.com>
2022-05-06 18:34:55 +0900

    0506

--- js/weather.js
+++ js/weather.js
@@ -6,7 +6,7 @@ function onGeoOk(position){
   const url =  `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
   .then((response) => response.json())
-  .then((data) => {
+  .then((data) => {   
     const weather = document.querySelector("#weather span:first-child");
     const city = document.querySelector("#weather span:last-child");
     city.innerText = data.name;
