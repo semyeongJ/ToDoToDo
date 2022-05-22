4c1944461a080968c1d4985e4aa49eff49c1af6a
semyeongJ <94214574+semyeongJ@users.noreply.github.com>
2022-05-23 00:05:36 +0900

    final

--- js/greetings.js
+++ js/greetings.js
@@ -5,28 +5,28 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
-function onLoginSubmit(event){
-  event.preventDefault()
+function onLoginSubmit(event) {
+  event.preventDefault();
   // loginForm.classList.add(HIDDEN_CLASSNAME);
-  loginWindow.style = "display: none;" // 이름 입력되면 디스플레이 숨김
+  loginWindow.style = "display: none;"; // 이름 입력되면 디스플레이 숨김
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-  paintGreeting(username)
+  paintGreeting(username);
 }
 
-function paintGreeting(username){
+function paintGreeting(username) {
   greeting.innerText = `Have a Good Day, ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-const savedUsername = localStorage.getItem(USERNAME_KEY)
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if(savedUsername === null){
+if (savedUsername === null) {
   //show the form
   loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginWindow.addEventListener("submit",onLoginSubmit);
+  loginWindow.addEventListener("submit", onLoginSubmit);
 } else {
   loginWindow.style.display = "none";
-  //show the greetings 
-  paintGreeting(savedUsername)
+  //show the greetings
+  paintGreeting(savedUsername);
 }
